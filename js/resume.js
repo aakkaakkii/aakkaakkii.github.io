5dcb46705cd8b1dc21488b47a2927a9c44b29097
aakkaakkii <aakkaakkii1997@gmail.com>
2019-02-09 14:19:35 +0400

    localisation & google analytics

--- js/resume.js
+++ js/resume.js
@@ -5,7 +5,7 @@ let lang = 'en';
 
     document.cookie.split('; ').forEach(function (d) {
         let data = d.split('=');
-        if (data[0]==='lang')
+        if (data[0] === 'lang')
             lang = data[1];
     });
 
@@ -48,27 +48,27 @@ let lang = 'en';
         });
     });
 
-   /* $.ajax({
-        url:'https://aakkaakkii.github.io/data/experience.json',
-        method: 'GET',
-        success: function (data) {
-            console.log(data)
-        }
+    /* $.ajax({
+         url:'https://aakkaakkii.github.io/data/experience.json',
+         method: 'GET',
+         success: function (data) {
+             console.log(data)
+         }
 
-    })*/
+     })*/
 
-    $.getJSON('data/'+lang+'/education.json', function (json) {
+    $.getJSON('data/' + lang + '/education.json', function (json) {
         json.forEach(function (d) {
             $('#education-section').append(
                 '<div id="education-container">\n' +
                 '                <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">\n' +
                 '                    <div class="resume-content">\n' +
                 '                        <h3 class="mb-0">' + d.university + '</h3>\n' +
-                '                        <div class="subheading mb-3">'+ d.degree +'</div>\n' +
-                '                        <div>'+ d.description +'</div>\n' +
+                '                        <div class="subheading mb-3">' + d.degree + '</div>\n' +
+                '                        <div>' + d.description + '</div>\n' +
                 '                    </div>\n' +
                 '                    <div class="resume-date text-md-right">\n' +
-                '                        <span class="text-primary">'+ d.date +'</span>\n' +
+                '                        <span class="text-primary">' + d.date + '</span>\n' +
                 '                    </div>\n' +
                 '                </div>\n' +
                 '            </div>'
@@ -76,22 +76,40 @@ let lang = 'en';
         });
     });
 
-    $.getJSON('data/'+lang+'/experience.json', function (json) {
+    $.getJSON('data/' + lang + '/experience.json', function (json) {
         json.forEach(function (d) {
             $('#experience-container').append(
                 '<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">\n' +
                 '                <div class="resume-content">\n' +
-                '                    <h3 class="mb-0">'+ d.position +'</h3>\n' +
-                '                    <div class="subheading mb-3">'+ d.company +'</div>\n' +
-                '                    <p>'+ d.description +'</p>\n' +
+                '                    <h3 class="mb-0">' + d.position + '</h3>\n' +
+                '                    <div class="subheading mb-3">' + d.company + '</div>\n' +
+                '                    <p>' + d.description + '</p>\n' +
                 '                </div>\n' +
                 '                <div class="resume-date text-md-right">\n' +
-                '                    <span class="text-primary">'+ d.date +'</span>\n' +
+                '                    <span class="text-primary">' + d.date + '</span>\n' +
                 '                </div>\n' +
                 '            </div>'
             );
         });
     });
+
+    $.getJSON('data/' + lang + '/about&interface.json', function (json) {
+        $('#about-me').html(json[0].description);
+        if (lang === "ge") {
+            $('#name-section').css({"padding-bottom":"30px"});
+            $('#about-item').html(json[0].about);
+            $('#experience-item').html(json[0].experience);
+            $('#education-item').html(json[0].education);
+            $('#skills-item').html(json[0].skills);
+            $('#navbar-name').html(json[0].firstName + ' ' + json[0].lastName);
+            $('#first_name').html(json[0].firstName);
+            $('#last_name').html(json[0].lastName);
+            $('#experience-title').html(json[0].experience);
+            $('#education-title').html(json[0].education);
+            $('#skills-title').html(json[0].skills);
+            $('#skills-sub-title').html(json[0].skillsSubTitle);
+        }
+    })
 
 })(jQuery); // End of use strict
 
